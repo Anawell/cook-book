19378d5b3f9436c461c0d59f195bdaead3d66acd
Anaelle <thomas.anawel@gmail.com>
2020-10-08 11:02:00 +0200

    feat: creation Recettes routes to show, modify and delete

--- routes/recettes.js
+++ routes/recettes.js
@@ -35,7 +35,7 @@ router.get('/ajouter', async (req, res) => {
 });
 
 // Creation new recette route
-router.post('/', async (req, res) => { // Multer nous permet de dire qu'on upload une image de name 'cover' et créé la variable "file"
+router.post('/', async (req, res) => { 
     const recette = new Recette({
         title: req.body.title,
         category: req.body.category,
@@ -46,17 +46,116 @@ router.post('/', async (req, res) => { // Multer nous permet de dire qu'on uploa
         listIngredients: req.body.listIngredients,
         steps: req.body.steps
     })
-
     saveCover(recette, req.body.cover);
-
     try {
         const newRecette = await recette.save(); // si la recette a bien été enregistrée dans la BDD
-        //res.redirect(`recettes/${newRecette.id}`);
-        res.redirect(`recettes`);
+        res.redirect(`/recettes/${newRecette.id}`);
     } catch {
         renderNewPage(res, new Recette(), true); // on réaffiche la page listing des recettes si erreur + true pour activer le hasError
     }
 });
+
+// Display a single recette
+router.get('/:id', async (req, res) => {
+    try {
+        const recette = await Recette.findById(req.params.id).populate('ingredient').exec(); 
+        // si on ne met pas le .populate, la DB renvoie l'id de l'ingredient, et pas son nom! Avec populate, il renvoie un objet contenant toutes les infos de l'ingredient
+        res.render('recettes/afficher', {
+            recette: recette
+        })
+    } catch {
+        res.redirect('/');
+    }
+});
+
+// Edit a single recette
+router.get('/:id/editer', async (req, res) => {
+    try {
+        recette = await Recette.findById(req.params.id)
+        renderEditPage(res, recette);
+    } catch {
+        res.redirect('/');
+    }
+    
+});
+
+// Modify a single recette
+router.put('/:id', async (req, res) => { 
+    let recette;
+    try {
+        recette = await Recette.findById(req.params.id) 
+        recette.title = req.body.title;
+        recette.category = req.body.category;
+        recette.personCount = req.body.personCount;
+        recette.timeCount = req.body.timeCount;
+        recette.difficultyCount = req.body.difficultyCount;
+        recette.ingredient = req.body.ingredient;
+        recette.listIngredients = req.body.listIngredients;
+        recette.steps = req.body.steps;
+        if (req.body.cover != null && req.body.cover !== '') {
+            saveCover(recette, req.body.cover);
+        }
+        await recette.save();
+        res.redirect(`/recettes/${recette.id}`);
+    } catch (err) {
+        console.log(err);
+        if (recette != null) {
+            renderEditPage(res, recette, true); 
+        } else {
+            res.redirect('/');
+        }
+    }
+});
+
+// Delete a recette
+router.delete('/:id', async (req, res) => {
+    let recette;
+    try {
+        recette = await Recette.findById(req.params.id);
+        await recette.remove();
+        res.redirect('/recettes');
+    } catch {
+        if (recette != null) {
+            res.render('recettes/afficher', {
+                recette: recette,
+                errorMessage: 'Impossible de supprimer la recette'
+            });
+        } else {
+            res.redirect('/');
+        }
+    }
+});
+
+async function renderNewPage(res, recette, hasError = false) {
+    renderFormPage(res, recette, 'ajouter', hasError);
+}
+
+async function renderEditPage(res, recette, hasError = false) {
+    renderFormPage(res, recette, 'editer', hasError);
+}
+
+async function renderFormPage(res, recette, form, hasError = false) {
+    try {
+        const ingredients = await Ingredient.find({});
+        const params = {
+            ingredients: ingredients,
+            recette: recette
+        }
+        if (hasError) {
+            if (form === 'editer') {
+                params.errorMessage = `Erreur pendant l'édition de la recette`;
+            } else if (form === 'ajouter') {
+                params.errorMessage = `Erreur pendant la création de la recette`;
+            } else {
+                params.errorMessage = `Erreur pendant l'exécution du script`;
+            }
+            
+        }
+        res.render(`recettes/${form}`, params);
+    } catch {
+        res.redirect('/recettes');
+    }
+}
 
 function saveCover(recette, encodedCover) { // FileEncode de FilePond encode l'image en base64 (string) puis la store dans un objet JSON.
     if (encodedCover == null) return;
@@ -66,22 +165,6 @@ function saveCover(recette, encodedCover) { // FileEncode de FilePond encode l'i
         // la propriété data est une propriété comprise dans l'objet JSON et contient la string Base64 - voir doc File Encode Plugin
         // On la transforme en type Buffer en précisant son type d'origine (Base64)
         recette.coverImageType = cover.type; // on obtient le MIME type
-    }
-}
-
-async function renderNewPage(res, recette, hasError = false) {
-    try {
-        const ingredients = await Ingredient.find({});
-        const params = {
-            ingredients: ingredients,
-            recette: recette
-        }
-        if (hasError) {
-            params.errorMessage = 'Erreur pendant la création de la nouvelle recette'
-        }
-        res.render('recettes/ajouter', params);
-    } catch {
-        res.redirect('/recettes');
     }
 }
 
